bdd816e8b8aebe3d56768ea5826e42fb43a0c45c
Richard Neumann <neumann@paulmann.de>
2023-09-22 13:16:54 +0200

    Implement RST

--- src/packet.rs
+++ src/packet.rs
@@ -1,13 +1,14 @@
 pub mod ack;
 pub mod nak;
+pub mod rst;
 
 pub enum Packet {
     Ack(ack::Ack),
     Nak(nak::Nak),
+    Rst(rst::Rst),
     /*
     Data(data::Data),
     Error(error::Error),
-    Rst(rst::Rst),
     RstAck(rst_ack::RstAck),
     */
 }
---
+++ src/packet/rst.rs
@@ -0,0 +1,29 @@
+use std::fmt::{Display, Formatter};
+
+pub const HEADER: u8 = 0xC0;
+pub const CRC: u16 = 0x38BC;
+
+/// Requests the NCP to perform a software reset (valid even if the NCP is in the FAILED state).
+pub struct Rst;
+
+impl Rst {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Display for Rst {
+    /// Display the RST packet
+    ///
+    /// # Examples
+    /// ```
+    /// use ashv2::packet::rst::Rst;
+    ///
+    /// let rst = Rst::new();
+    /// assert_eq!(&rst.to_string(), "RST()");
+    /// ```
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "RST()")
+    }
+}
