c6d51ca35a718cd0d8b626c87563529a08bdbed8
Richard Neumann <neumann@paulmann.de>
2023-09-22 13:47:23 +0200

    Add Display and doctests

--- src/packet/rst_ack.rs
+++ src/packet/rst_ack.rs
@@ -22,14 +22,49 @@ impl RstAck {
         }
     }
 
+    /// Returns the protocol version.
+    ///
+    /// This is statically set to 0x02 (2) for ASHv2.
+    ///
+    /// # Examples
+    /// ```
+    /// use ashv2::packet::rst_ack::{RstAck, VERSION};
+    ///
+    /// let rst_ack = RstAck::new( 0x02, 0x02, 0x9B7B);
+    /// assert_eq!(rst_ack.version(), VERSION);
+    /// ```
     #[must_use]
     pub const fn version(&self) -> u8 {
         self.version
     }
 
+    /// Returns the reset code.
+    ///
+    /// # Examples
+    /// ```
+    /// use ashv2::packet::rst_ack::{ResetCode, RstAck, VERSION};
+    ///
+    /// let rst_ack = RstAck::new( 0x02, 0x02, 0x9B7B);
+    /// assert_eq!(rst_ack.reset_code(), Some(ResetCode::PowerOn));
+    /// ```
     #[must_use]
     pub fn reset_code(&self) -> Option<ResetCode> {
         ResetCode::from_u8(self.reset_code)
+    }
+}
+
+impl Display for RstAck {
+    /// Formats the RSTACK as a String..
+    ///
+    /// # Examples
+    /// ```
+    /// use ashv2::packet::rst_ack::{ResetCode, RstAck, VERSION};
+    ///
+    /// let rst_ack = RstAck::new( 0x02, 0x02, 0x9B7B);
+    /// assert_eq!(&rst_ack.to_string(), "RSTACK(0x02, 0x02)");
+    /// ```
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "RSTACK({:#04x}, {:#04x})", self.version, self.reset_code)
     }
 }
 
