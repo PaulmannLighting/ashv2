e8be79a495e9429c2376def446bbdc757ed44d86
Richard Neumann <neumann@paulmann.de>
2023-09-22 18:01:22 +0200

    Implement Stuffer

--- src/protocol/stuffing.rs
+++ src/protocol/stuffing.rs
@@ -7,6 +7,57 @@ const CANCEL: u8 = 0x1A;
 const RESERVED_BYTES: [u8; 6] = [FLAG, ESCAPE, X_ON, X_OFF, SUBSTITUTE, CANCEL];
 const COMPLEMENT_BIT: u8 = 1 << 5;
 
+pub struct Stuffer<T>
+where
+    T: Iterator<Item = u8>,
+{
+    bytes: T,
+    next: Option<u8>,
+}
+
+/// Stuff bytes.
+///
+/// # Examples
+/// ```
+/// use ashv2::protocol::stuffing::Stuffer;
+///
+/// let original = vec![0x7E, 0x11, 0x13, 0x18, 0x1A, 0x7D];
+/// let target = vec![0x7D, 0x5E, 0x7D, 0x31, 0x7D, 0x33, 0x7D, 0x38, 0x7D, 0x3A, 0x7D, 0x5D];
+/// let stuffer = Stuffer::new(original.into_iter());
+/// let stuffed: Vec<u8> = stuffer.collect();
+/// assert_eq!(stuffed, target);
+/// ```
+impl<T> Stuffer<T>
+where
+    T: Iterator<Item = u8>,
+{
+    pub const fn new(bytes: T) -> Self {
+        Self { bytes, next: None }
+    }
+}
+
+impl<T> Iterator for Stuffer<T>
+where
+    T: Iterator<Item = u8>,
+{
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next) = self.next.take() {
+            Some(next)
+        } else if let Some(byte) = self.bytes.next() {
+            if RESERVED_BYTES.contains(&byte) {
+                self.next = Some(byte ^ COMPLEMENT_BIT);
+                Some(ESCAPE)
+            } else {
+                Some(byte)
+            }
+        } else {
+            None
+        }
+    }
+}
+
 pub struct Unstuffer<T>
 where
     T: Iterator<Item = u8>,
