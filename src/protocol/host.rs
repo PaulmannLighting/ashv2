7fc1ad7787e0adbd0aa3fa27677a90bbd8a0e9c2
Richard Neumann <neumann@paulmann.de>
2023-09-25 11:17:42 +0200

    Use serialport library

--- src/protocol/host.rs
+++ src/protocol/host.rs
@@ -1,31 +1,29 @@
 use crate::packet::Packet;
-use std::io::{Error, Read, Write};
+use serialport::SerialPort;
+use std::io::Error;
 
 const MIN_BUF_CAPACITY: usize = 4;
 
-pub struct Host<R, W>
+pub struct Host<S>
 where
-    R: Read,
-    W: Write,
+    S: SerialPort,
 {
-    reader: R,
-    writer: W,
+    serial_port: S,
 }
 
-impl<R, W> Host<R, W>
+impl<S> Host<S>
 where
-    R: Read,
-    W: Write,
+    S: SerialPort,
 {
-    pub fn new(reader: R, writer: W) -> Self {
-        Self { reader, writer }
+    pub const fn new(serial_port: S) -> Self {
+        Self { serial_port }
     }
 
     pub fn read_packet(&mut self) -> Result<Packet, Error> {
         //let mut buffer = Vec::with_capacity(MIN_BUF_CAPACITY);
 
         let mut header: [u8; 1] = [0];
-        self.reader.read_exact(&mut header)?;
+        self.serial_port.read_exact(&mut header)?;
 
         Err(Error::last_os_error())
     }
