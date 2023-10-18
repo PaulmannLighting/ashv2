2ae90ca49b474ae2418489364d1cf8d131601f28
Richard Neumann <neumann@paulmann.de>
2023-10-18 09:10:30 +0200

    Use trait for masking

--- src/protocol/randomization.rs
+++ src/protocol/randomization.rs
@@ -1,16 +1,23 @@
+use std::iter::{Map, Zip};
+
 const DEFAULT_MASK: u8 = 0xB8;
 const DEFAULT_SEED: u8 = 0x42;
 const DEFAULT_FLAG_BIT: u8 = 0x01;
 
-/// Masks a byte stream with pseudo-random numbers.
-pub fn mask(bytes: impl Iterator<Item = u8>) -> impl Iterator<Item = u8> {
-    bytes
-        .zip(MaskGenerator::default())
-        .map(|(byte, mask)| byte ^ mask)
+type MaskIterator<T> = Map<Zip<T, MaskGenerator>, fn((u8, u8)) -> u8>;
+
+pub trait Mask: Iterator<Item = u8> + Sized {
+    /// Masks a byte stream with pseudo-random numbers.
+    fn mask(self) -> MaskIterator<Self> {
+        self.zip(MaskGenerator::default())
+            .map(|(byte, mask)| byte ^ mask)
+    }
 }
 
+impl<T> Mask for T where T: Iterator<Item = u8> {}
+
 #[derive(Clone, Debug, Eq, PartialEq)]
-struct MaskGenerator {
+pub struct MaskGenerator {
     random: u8,
     flag_bit: u8,
     mask: u8,
@@ -52,7 +59,7 @@ impl Iterator for MaskGenerator {
 
 #[cfg(test)]
 mod tests {
-    use super::{mask, MaskGenerator};
+    use super::{Mask, MaskGenerator};
 
     #[test]
     fn test_mask_generator() {
@@ -64,18 +71,18 @@ mod tests {
     #[test]
     fn test_mask_with_version_command() {
         let bytes = vec![0x00, 0x00, 0x00, 0x02];
-        let masked: Vec<_> = mask(bytes.clone().into_iter()).collect();
+        let masked: Vec<_> = bytes.clone().into_iter().mask().collect();
         assert_eq!(masked, vec![0x42, 0x21, 0xA8, 0x56]);
-        let unmasked: Vec<_> = mask(masked.into_iter()).collect();
+        let unmasked: Vec<_> = masked.into_iter().mask().collect();
         assert_eq!(unmasked, bytes);
     }
 
     #[test]
     fn test_mask_with_version_response() {
         let bytes = vec![0x00, 0x80, 0x00, 0x02, 0x02, 0x11, 0x30];
-        let masked: Vec<_> = mask(bytes.clone().into_iter()).collect();
+        let masked: Vec<_> = bytes.clone().into_iter().mask().collect();
         assert_eq!(masked, vec![0x42, 0xA1, 0xA8, 0x56, 0x28, 0x04, 0x82]);
-        let unmasked: Vec<_> = mask(masked.into_iter()).collect();
+        let unmasked: Vec<_> = masked.into_iter().mask().collect();
         assert_eq!(unmasked, bytes);
     }
 }
