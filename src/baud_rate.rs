320115c3f8b2ab2e4aa6edb8e52db3475db15ccc
Richard Neumann <neumann@paulmann.de>
2023-10-27 09:34:11 +0200

    Clean up deps

--- src/baud_rate.rs
+++ src/baud_rate.rs
@@ -1,11 +1,7 @@
 use num_derive::{FromPrimitive, ToPrimitive};
-use num_traits::{FromPrimitive, ToPrimitive};
-use std::str::FromStr;
-use strum::IntoStaticStr;
+use num_traits::ToPrimitive;
 
-#[derive(
-    Clone, Debug, Eq, Ord, PartialEq, PartialOrd, FromPrimitive, ToPrimitive, IntoStaticStr,
-)]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, FromPrimitive, ToPrimitive)]
 pub enum BaudRate {
     RstCts = 115_200,
     XOnXOff = 57_600,
@@ -16,14 +12,5 @@ impl From<BaudRate> for u32 {
         baud_rate
             .to_u32()
             .expect("could not convert baud rate to u32")
-    }
-}
-
-impl FromStr for BaudRate {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Self::from_u32(s.parse::<u32>().map_err(|error| error.to_string())?)
-            .ok_or_else(|| "unsupported baud rate".to_string())
     }
 }
