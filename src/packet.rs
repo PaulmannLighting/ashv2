9861f4ee5a9425db1673e459e433e2bc9181a3a5
Richard Neumann <neumann@paulmann.de>
2024-03-28 22:49:41 +0100

    Tighten up API

--- src/packet.rs
+++ src/packet.rs
@@ -16,7 +16,7 @@ pub use rst_ack::RstAck;
 use std::fmt::{Debug, Display, Formatter};
 
 // In the wost-case, all frame bytes are stuffed (*2) and we append the FLAG byte (+1).
-pub const MAX_FRAME_SIZE: usize = (METADATA_SIZE + MAX_PAYLOAD_SIZE) * 2 + 1;
+const MAX_FRAME_SIZE: usize = (METADATA_SIZE + MAX_PAYLOAD_SIZE) * 2 + 1;
 pub type FrameBuffer = heapless::Vec<u8, MAX_FRAME_SIZE>;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
