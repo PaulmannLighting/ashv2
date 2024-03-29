cea5558c891b10a276719fdabca0d04291b24ce3
Richard Neumann <neumann@paulmann.de>
2024-03-29 10:40:37 +0100

    Remove HRTB

--- src/ash_write.rs
+++ src/ash_write.rs
@@ -10,20 +10,20 @@ pub trait AshWrite: Write {
     ///
     /// # Errors
     /// Returns an [`Error`] if any I/O error occurs.
-    fn write_frame<F>(&mut self, frame: &F) -> Result<()>
+    fn write_frame<'frame, F>(&mut self, frame: &'frame F) -> Result<()>
     where
         F: Frame,
-        for<'a> &'a F: IntoIterator<Item = u8>;
+        &'frame F: IntoIterator<Item = u8>;
 }
 
 impl<T> AshWrite for T
 where
     T: Write,
 {
-    fn write_frame<F>(&mut self, frame: &F) -> Result<()>
+    fn write_frame<'frame, F>(&mut self, frame: &'frame F) -> Result<()>
     where
         F: Frame,
-        for<'a> &'a F: IntoIterator<Item = u8>,
+        &'frame F: IntoIterator<Item = u8>,
     {
         debug!("Writing frame: {frame}");
         trace!("{frame:#04X?}");
