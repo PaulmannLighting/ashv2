5e28bfee24b7080765697eeea9147576e5848c10
Richard Neumann <neumann@paulmann.de>
2024-04-22 08:59:49 +0200

    Add documentation of the response system

--- src/protocol/response.rs
+++ src/protocol/response.rs
@@ -3,6 +3,14 @@ use std::future::Future;
 
 use crate::Error;
 
+/// Result of handling an event.
+///
+/// This enum is returned from [`Handler::handle`] to indicate the outcome of handling the respective event.
+///
+/// * [`HandleResult::Completed`] indicates that the handler is done and does not expect any more data.  
+/// * [`HandleResult::Continue`] indicates that the handler is expecting more data.
+/// * [`HandleResult::Failed`] indicates that the handler was unable to process the passed data and cannot continue.
+/// * [`HandleResult::Reject`] indicates that the handler was unable to process the passed data and is expecting more data.
 #[derive(Debug)]
 pub enum HandleResult {
     Completed,
@@ -11,18 +19,32 @@ pub enum HandleResult {
     Reject,
 }
 
+/// Events sent to a [`Handler`].
+///
+/// * [`Event::TransmissionCompleted`] indicates that the requested payload has been transmitted completely.
+/// * [`Event::DataReceived`] indicates that the listener has received a data packet that it is not forwarding to the handler for processing.
 #[derive(Debug)]
 pub enum Event<'data> {
     TransmissionCompleted,
     DataReceived(Result<&'data [u8], Error>),
 }
 
+/// Handle ASHv2 protocol events.
 pub trait Handler: Debug + Send + Sync {
+    /// Handle the incoming  [`Event`] and return an appropriate [`HandleResult`].
     fn handle(&self, event: Event) -> HandleResult;
+
+    /// Abort the current transaction, resulting in an erroneous state.
     fn abort(&self, error: Error);
+
+    /// Wake the underlying [`Waker`](std::task::Waker) to complete the respective [`Future`].
     fn wake(&self);
 }
 
+/// A response to an NCP request.
+///
+/// This is a composite trait consisting of a [`Future`] and a [`Handler`] implementation.
+/// The Future must output [`Result<Self::Result, Self::Error>`].
 pub trait Response: Future<Output = Result<Self::Result, Self::Error>> + Handler
 where
     Self::Error: From<Error>,
