d5cff860b3dfcf6862108c7ab466d1bfba19dfbd
Richard Neumann <neumann@paulmann.de>
2024-07-25 14:47:27 +0200

    Undo last commit since it remove stuffing

--- src/ash_write.rs
+++ src/ash_write.rs
@@ -25,7 +25,11 @@ where
     {
         debug!("Writing frame: {frame}");
         trace!("{frame:#04X?}");
-        self.write_all(frame.bytes().as_ref())?;
+
+        for byte in frame.bytes().as_ref().iter().copied().stuff() {
+            self.write_all(&[byte])?;
+        }
+
         self.write_all(&[FLAG])?;
         self.flush()
     }
