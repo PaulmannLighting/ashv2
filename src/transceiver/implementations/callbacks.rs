cb5bfca80a1676b9e5935b671efc5e66022320ed
Richard Neumann <neumann@paulmann.de>
2024-10-02 09:47:42 +0200

    Implement handle_callbacks()

--- src/transceiver/implementations/callbacks.rs
+++ src/transceiver/implementations/callbacks.rs
@@ -2,6 +2,16 @@ use crate::Transceiver;
 
 impl Transceiver {
     pub(in crate::transceiver) fn handle_callbacks(&mut self) -> std::io::Result<()> {
-        todo!("Implement the callback handler");
+        self.buffers.response.clear();
+
+        while let Some(callback) = self.receive()? {
+            self.handle_packet(&callback)?;
+        }
+
+        if self.buffers.response.is_empty() {
+            Ok(())
+        } else {
+            self.channels.callback(self.buffers.response.clone().into())
+        }
     }
 }
