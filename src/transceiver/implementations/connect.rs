c0db1d5d761a76a89d90250fd8aca590d565e1a9
Richard Neumann <neumann@paulmann.de>
2024-10-02 15:47:35 +0200

    Wait for packet

--- src/transceiver/implementations/connect.rs
+++ src/transceiver/implementations/connect.rs
@@ -12,19 +12,23 @@ where
     pub(in crate::transceiver) fn connect(&mut self) -> std::io::Result<()> {
         debug!("Connecting to NCP...");
         let start = SystemTime::now();
+        let mut attempts: usize = 0;
 
         loop {
+            attempts += 1;
             self.rst()?;
 
-            let Some(packet) = self.receive()? else {
-                continue;
+            let packet = loop {
+                if let Some(packet) = self.receive()? {
+                    break packet;
+                }
             };
 
             match packet {
                 Packet::RstAck(rst_ack) => {
                     debug!("Received RSTACK: {rst_ack}");
                     self.state.status = Status::Connected;
-                    info!("ASHv2 connection established.");
+                    info!("ASHv2 connection established after {attempts} attempts.");
 
                     if let Ok(elapsed) = start.elapsed() {
                         debug!("Establishing connection took {elapsed:?}");
