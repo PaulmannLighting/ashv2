1f3bd0909bb5baedea39d533b6753f90a77be42d
Richard Neumann <neumann@paulmann.de>
2024-10-02 10:30:37 +0200

    Refactor connect()

--- src/transceiver/implementations/connect.rs
+++ src/transceiver/implementations/connect.rs
@@ -1,23 +1,26 @@
 use crate::packet::Packet;
 use crate::status::Status;
 use crate::Transceiver;
-use log::debug;
+use log::{debug, info};
 use std::time::SystemTime;
 
 impl Transceiver {
     pub(in crate::transceiver) fn connect(&mut self) -> std::io::Result<()> {
         debug!("Connecting to NCP...");
         let start = SystemTime::now();
+        let mut attempts: usize = 0;
 
         loop {
+            attempts += 1;
             self.rst()?;
 
             if let Packet::RstAck(rst_ack) = self.read_packet()? {
                 debug!("Received RSTACK: {rst_ack}");
                 self.state.status = Status::Connected;
+                info!("Connection established after {attempts} attempts.");
 
                 if let Ok(elapsed) = start.elapsed() {
-                    debug!("Connection established after {elapsed:?}");
+                    debug!("Establishing connection took {elapsed:?}");
                 }
 
                 return Ok(());
