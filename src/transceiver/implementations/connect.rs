ce5251828b850b5fa131cf64e2c69c79e2e14078
Richard Neumann <neumann@paulmann.de>
2024-10-02 15:42:44 +0200

    Fix establishing connection

--- src/transceiver/implementations/connect.rs
+++ src/transceiver/implementations/connect.rs
@@ -12,22 +12,30 @@ where
     pub(in crate::transceiver) fn connect(&mut self) -> std::io::Result<()> {
         debug!("Connecting to NCP...");
         let start = SystemTime::now();
-        let mut attempts: usize = 0;
 
         loop {
-            attempts += 1;
             self.rst()?;
 
-            if let Packet::RstAck(rst_ack) = self.read_packet()? {
-                debug!("Received RSTACK: {rst_ack}");
-                self.state.status = Status::Connected;
-                info!("Connection established after {attempts} attempts.");
+            let Some(packet) = self.receive()? else {
+                continue;
+            };
 
-                if let Ok(elapsed) = start.elapsed() {
-                    debug!("Establishing connection took {elapsed:?}");
+            match packet {
+                Packet::RstAck(rst_ack) => {
+                    debug!("Received RSTACK: {rst_ack}");
+                    self.state.status = Status::Connected;
+                    info!("ASHv2 connection established.");
+
+                    if let Ok(elapsed) = start.elapsed() {
+                        debug!("Establishing connection took {elapsed:?}");
+                    }
+
+                    return Ok(());
                 }
-
-                return Ok(());
+                other => {
+                    debug!("Expected RSTACK but got: {other}");
+                    continue;
+                }
             }
         }
     }
