a49c7556e6f63cf40db4c8535e0ac0c6881422b0
Richard Neumann <neumann@paulmann.de>
2024-10-03 11:18:11 +0200

    Add docs to Code

--- src/code.rs
+++ src/code.rs
@@ -1,16 +1,26 @@
 use std::fmt::{Display, Formatter};
 
+/// Reset and error codes.
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[repr(u8)]
 pub enum Code {
+    /// Reset: Unknown reason
     UnknownReason = 0x00,
+    /// Reset: External
     External = 0x01,
+    /// Reset: Power-on
     PowerOn = 0x02,
+    /// Reset: Watchdog
     Watchdog = 0x03,
+    /// Reset: Assert
     Assert = 0x06,
+    /// Reset: Boot loader
     Bootloader = 0x09,
+    /// Reset: Software
     Software = 0x0B,
+    /// Error: Exceeded maximum ACK timeout count
     ExceededMaximumAckTimeoutCount = 0x51,
+    /// Chip-specific error reset code
     ChipSpecific = 0x80,
 }
 
