54bf8e22995451aaca14da7594813ee48ccd3fb9
Richard Neumann <neumann@paulmann.de>
2024-10-07 18:38:19 +0200

    Create decoder on the fly

--- src/host.rs
+++ src/host.rs
@@ -6,23 +6,16 @@ use tokio_util::codec::Decoder;
 
 /// A host that communicates with the NCP.
 #[derive(Debug)]
-pub struct Host<T>
-where
-    T: Decoder,
-{
-    decoder: T,
+pub struct Host {
     sender: SyncSender<Request>,
     buffer: BytesMut,
 }
 
-impl<T> Host<T>
-where
-    T: Decoder,
-{
+impl Host {
     /// Create a new host.
-    pub fn new(decoder: T, sender: SyncSender<Request>) -> Self {
+    #[must_use]
+    pub fn new(sender: SyncSender<Request>) -> Self {
         Self {
-            decoder,
             sender,
             buffer: BytesMut::new(),
         }
@@ -36,12 +29,18 @@ where
     ///
     /// Returns a [`Decoder::Error`] if the request could not be sent
     /// or the response could not be received or decoded.
-    pub fn communicate(&mut self, payload: &[u8]) -> Result<T::Item, T::Error> {
+    pub fn communicate<T>(&mut self, payload: &[u8]) -> Result<T::Item, T::Error>
+    where
+        T: Decoder + Default,
+    {
         self.buffer.clear();
+
         let (request, response) = Request::new(payload.into());
         self.sender.send(request).map_err(|_| {
             std::io::Error::new(ErrorKind::BrokenPipe, "ASHv2: Failed to send request.")
         })?;
+
+        let mut decoder = T::default();
 
         loop {
             if self.buffer.is_empty() {
@@ -53,7 +52,7 @@ where
                 })?);
             }
 
-            if let Some(item) = self.decoder.decode(&mut self.buffer)? {
+            if let Some(item) = decoder.decode(&mut self.buffer)? {
                 self.buffer.clear();
                 return Ok(item);
             }
