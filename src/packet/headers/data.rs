4da1556bae442b5b7247167a273f0a53e2f2fc47
Richard Neumann <neumann@paulmann.de>
2024-10-07 10:13:34 +0200

    Remove unused DEFAULT variant

--- src/packet/headers/data.rs
+++ src/packet/headers/data.rs
@@ -8,7 +8,6 @@ pub struct Data(u8);
 
 bitflags! {
     impl Data: u8 {
-        const DEFAULT = 0b0000_0000;
         const FRAME_NUM = 0b0111_0000;
         const RETRANSMIT = 0b0000_1000;
         const ACK_NUM = 0b0000_0111;
@@ -19,7 +18,7 @@ impl Data {
     /// Creates a new data header.
     #[must_use]
     pub fn new(frame_num: WrappingU3, retransmit: bool, ack_num: WrappingU3) -> Self {
-        let mut ack = Self::DEFAULT;
+        let mut ack = Self::empty();
         ack |= Self::FRAME_NUM & Self::from_bits_retain(frame_num.as_u8() << FRAME_NUM_OFFSET);
         ack.set(Self::RETRANSMIT, retransmit);
         ack |= Self::ACK_NUM & Self::from_bits_retain(ack_num.as_u8());
