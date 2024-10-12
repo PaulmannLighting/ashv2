373c71ba112b727a74d8c63e928508bb878f25da
Richard Neumann <neumann@paulmann.de>
2024-10-12 19:06:42 +0200

    Use one continuously running thread

--- src/ash_framed.rs
+++ src/ash_framed.rs
@@ -4,26 +4,48 @@ use crate::Request;
 use shared_state::SharedState;
 use std::io::ErrorKind;
 use std::pin::Pin;
-use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::Relaxed;
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender, TrySendError};
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll, Waker};
-use std::thread::spawn;
+use std::task::{Context, Poll};
+use std::thread::{sleep, spawn, JoinHandle};
+use std::time::Duration;
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+
+const BURST: Duration = Duration::from_millis(100);
 
 /// A framed asynchronous `ASHv2` host.
 #[derive(Debug)]
 pub struct AshFramed<const BUF_SIZE: usize> {
     sender: SyncSender<Request>,
+    running: Arc<AtomicBool>,
     state: Arc<Mutex<SharedState>>,
+    receiver: Option<JoinHandle<()>>,
 }
 
 impl<const BUF_SIZE: usize> AshFramed<BUF_SIZE> {
     /// Create a new `AshFramed` instance.
     #[must_use]
     pub fn new(sender: SyncSender<Request>) -> Self {
+        let running = Arc::new(AtomicBool::new(true));
+        let state = Arc::new(Mutex::new(SharedState::default()));
+        let receiver = spawn_reader(running.clone(), state.clone());
         Self {
             sender,
-            state: Arc::new(Mutex::new(SharedState::default())),
+            running,
+            state,
+            receiver: Some(receiver),
+        }
+    }
+}
+
+impl<const BUF_SIZE: usize> Drop for AshFramed<BUF_SIZE> {
+    fn drop(&mut self) {
+        self.running.store(false, Relaxed);
+
+        if let Some(receiver) = self.receiver.take() {
+            receiver.join().expect("thread panicked");
         }
     }
 }
@@ -31,28 +53,27 @@ impl<const BUF_SIZE: usize> AshFramed<BUF_SIZE> {
 impl<const BUF_SIZE: usize> AsyncWrite for &AshFramed<BUF_SIZE> {
     fn poll_write(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        _cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<std::io::Result<usize>> {
-        let result = self.state.lock().expect("mutex poisoned").sent_bytes.take();
+        let len = buf.len();
+        let (response_tx, response_rx) = sync_channel(BUF_SIZE);
+        let request = Request::new(buf.as_ref().into(), response_tx);
 
-        if let Some(result) = result {
-            return Poll::Ready(result);
+        match self.sender.try_send(request) {
+            Ok(()) => {
+                self.state
+                    .lock()
+                    .expect("mutex poisoned")
+                    .receiver
+                    .replace(response_rx);
+                Poll::Ready(Ok(len))
+            }
+            Err(error) => match error {
+                TrySendError::Full(_) => Poll::Ready(Err(ErrorKind::WouldBlock.into())),
+                TrySendError::Disconnected(_) => Poll::Ready(Err(ErrorKind::BrokenPipe.into())),
+            },
         }
-
-        if self.state.lock().expect("mutex poisoned").sending {
-            return Poll::Pending;
-        }
-
-        self.state.lock().expect("mutex poisoned").sending = true;
-        spawn_writer::<BUF_SIZE>(
-            self.sender.clone(),
-            cx.waker().clone(),
-            buf.to_vec().into_boxed_slice(),
-            self.state.clone(),
-        );
-
-        Poll::Pending
     }
 
     fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
@@ -71,55 +92,40 @@ impl<const BUF_SIZE: usize> AsyncRead for &AshFramed<BUF_SIZE> {
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        let receiver = self.state.lock().expect("mutex poisoned").receiver.take();
-
-        if let Some(receiver) = receiver {
-            spawn_reader(receiver, cx.waker().clone(), self.state.clone());
-        }
-
-        self.state
-            .lock()
-            .expect("mutex poisoned")
-            .result
-            .take()
-            .map_or_else(
-                || Poll::Pending,
-                |result| match result {
-                    Ok(data) => {
-                        buf.put_slice(&data);
-                        Poll::Ready(Ok(()))
-                    }
-                    Err(e) => Poll::Ready(Err(e)),
-                },
-            )
+        let mut lock = self.state.lock().expect("mutex poisoned");
+        lock.result.take().map_or_else(
+            || {
+                lock.waker.replace(cx.waker().clone());
+                Poll::Pending
+            },
+            |result| match result {
+                Ok(data) => {
+                    buf.put_slice(&data);
+                    Poll::Ready(Ok(()))
+                }
+                Err(error) => Poll::Ready(Err(error)),
+            },
+        )
     }
 }
 
-fn spawn_writer<const BUF_SIZE: usize>(
-    sender: SyncSender<Request>,
-    waker: Waker,
-    payload: Box<[u8]>,
-    state: Arc<Mutex<SharedState>>,
-) {
+fn spawn_reader(running: Arc<AtomicBool>, state: Arc<Mutex<SharedState>>) -> JoinHandle<()> {
     spawn(move || {
-        let len = payload.len();
-        let (response_tx, response_rx) = sync_channel(BUF_SIZE);
-        let request = Request::new(payload, response_tx);
-        let result = sender
-            .send(request)
-            .map(|()| len)
-            .map_err(|_| ErrorKind::BrokenPipe.into());
-        let mut lock = state.lock().expect("mutex poisoned");
-        lock.sent_bytes.replace(result);
-        lock.receiver.replace(response_rx);
-        lock.sending = false;
-        drop(lock);
-        waker.wake();
-    });
+        while running.load(Relaxed) {
+            let receiver = state.lock().expect("mutex poisoned").receiver.take();
+
+            if let Some(receiver) = receiver {
+                receive_loop(&receiver, &state);
+            } else {
+                sleep(BURST);
+                continue;
+            }
+        }
+    })
 }
 
-fn spawn_reader(receiver: Receiver<Box<[u8]>>, waker: Waker, state: Arc<Mutex<SharedState>>) {
-    spawn(move || loop {
+fn receive_loop(receiver: &Receiver<Box<[u8]>>, state: &Arc<Mutex<SharedState>>) {
+    loop {
         if let Ok(data) = receiver.recv() {
             state
                 .lock()
@@ -137,9 +143,11 @@ fn spawn_reader(receiver: Receiver<Box<[u8]>>, waker: Waker, state: Arc<Mutex<Sh
                 lock.result.replace(result);
             }
 
-            drop(lock);
-            waker.wake();
+            if let Some(waker) = lock.waker.take() {
+                waker.wake();
+            }
+
             return;
         }
-    });
+    }
 }
--- src/ash_framed/shared_state.rs
+++ src/ash_framed/shared_state.rs
@@ -1,20 +1,19 @@
 use std::sync::mpsc::Receiver;
+use std::task::Waker;
 
 #[derive(Debug, Default)]
 pub struct SharedState {
-    pub(super) sending: bool,
-    pub(super) sent_bytes: Option<std::io::Result<usize>>,
     pub(super) receiver: Option<Receiver<Box<[u8]>>>,
     pub(super) buffer: Vec<u8>,
     pub(super) result: Option<std::io::Result<Box<[u8]>>>,
+    pub(super) waker: Option<Waker>,
 }
 
 impl SharedState {
     pub fn reset(&mut self) {
-        self.sending = false;
-        self.sent_bytes = None;
         self.receiver = None;
         self.buffer.clear();
         self.result = None;
+        self.waker = None;
     }
 }
