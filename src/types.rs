a4e8e16617d2f82424782f7559eede41e5e30119
Richard Neumann <neumann@paulmann.de>
2024-10-13 19:34:03 +0200

    Update docstring

--- src/types.rs
+++ src/types.rs
@@ -3,7 +3,7 @@ use crate::packet::Data;
 /// In the wost-case, all frame bytes are stuffed (*2) and we append the FLAG byte (+1).
 const MAX_FRAME_SIZE: usize = Data::BUFFER_SIZE * 2 + 1;
 
-/// A stack-allocated buffer that can hold an `ASHv2` frame up to its maximum size.
+/// A stack-allocated buffer that can hold an `ASHv2` frame up to its maximum size with stuffing.
 pub type FrameBuffer = heapless::Vec<u8, MAX_FRAME_SIZE>;
 
 /// A stack-allocated buffer that can hold payload for an `ASHv2` DATA frame up to its maximum size.
