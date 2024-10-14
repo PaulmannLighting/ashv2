5e6404ad28f372ec922815f3c3a180192dc3a147
Richard Neumann <neumann@paulmann.de>
2024-10-14 12:18:33 +0200

    Update docs

--- src/types.rs
+++ src/types.rs
@@ -6,8 +6,8 @@ const MAX_FRAME_SIZE: usize = Data::BUFFER_SIZE * 2 + 1;
 /// A stack-allocated buffer that can hold an `ASHv2` frame up to its maximum size with stuffing.
 pub type FrameBuffer = heapless::Vec<u8, MAX_FRAME_SIZE>;
 
-/// The maximum size of a payload in an `ASHv2` DATA frame.
+/// The maximum payload size of an `ASHv2` [`Data`] frame.
 pub const MAX_PAYLOAD_SIZE: usize = Data::MAX_PAYLOAD_SIZE;
 
-/// A stack-allocated buffer that can hold payload for an `ASHv2` DATA frame up to its maximum size.
+/// A stack-allocated buffer that can hold payload for an `ASHv2` [`Data`] frame up to its maximum size.
 pub type Payload = heapless::Vec<u8, MAX_PAYLOAD_SIZE>;
