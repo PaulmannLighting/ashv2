cfc4f9ff38526f766aa34a33e64cd7c9d23ab1cc
Richard Neumann <neumann@paulmann.de>
2025-12-18 00:37:33 +0100

    Export `HexSlice`.
    TODO: Maybe outsource this?

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub use self::actor::{Actor, Error, Proxy};
 pub use self::baud_rate::BaudRate;
 pub use self::serial_port::{FlowControl, SerialPort, TTYPort, open};
 pub use self::types::Payload;
+pub use self::utils::HexSlice;
 
 const VERSION: u8 = 0x02;
 
