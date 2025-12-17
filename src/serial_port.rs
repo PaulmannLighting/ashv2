54a21f676c29bf24786346fb5f90af4aca25fad2
Richard Neumann <neumann@paulmann.de>
2025-12-18 00:06:41 +0100

    Return concrete `TTYPort` type.

--- src/serial_port.rs
+++ src/serial_port.rs
@@ -2,6 +2,7 @@
 
 use std::borrow::Cow;
 
+use serialport::TTYPort;
 pub use serialport::{FlowControl, SerialPort};
 
 use crate::BaudRate;
@@ -9,13 +10,14 @@ use crate::BaudRate;
 /// Opens a serial port depending on the local operating system.
 ///
 /// # Errors
+///
 /// For errors please refer to [`SerialPortBuilder::open_native()`](serialport::SerialPortBuilder::open_native())
 /// and [`serialport::new()`].
 pub fn open<'a>(
     path: impl Into<Cow<'a, str>>,
     baud_rate: BaudRate,
     flow_control: FlowControl,
-) -> serialport::Result<impl SerialPort> {
+) -> serialport::Result<TTYPort> {
     serialport::new(path, baud_rate.into())
         .flow_control(flow_control)
         .open_native()
