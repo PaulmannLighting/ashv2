5c8d183503d7a792989d912761055244eb934925
Richard Neumann <neumann@paulmann.de>
2026-01-22 13:24:21 +0100

    Make `Response` and `Error` static types.

--- src/actor/proxy.rs
+++ src/actor/proxy.rs
@@ -8,30 +8,21 @@ use tokio::sync::oneshot::{Receiver, channel};
 use crate::actor::message::Message;
 use crate::{HexSlice, Payload};
 
+type Response = Receiver<io::Result<()>>;
+type Error = SendError<Message>;
+
 /// `ASHv2` actor proxy.
 pub trait Proxy {
-    /// Response type for sending data to the `ASHv2` actor.
-    type Response;
-
-    /// Error type for sending data to the `ASHv2` actor.
-    type Error;
-
     /// Send data to the `ASHv2` actor.
     ///
     /// # Errors
     ///
     /// Returns an [`Error`] if sending the message fails.
-    fn send(
-        &self,
-        payload: Payload,
-    ) -> impl Future<Output = Result<Self::Response, Self::Error>> + Send;
+    fn send(&self, payload: Payload) -> impl Future<Output = Result<Response, Error>> + Send;
 }
 
 impl Proxy for Sender<Message> {
-    type Response = Receiver<io::Result<()>>;
-    type Error = SendError<Message>;
-
-    async fn send(&self, payload: Payload) -> Result<Self::Response, Self::Error> {
+    async fn send(&self, payload: Payload) -> Result<Response, Error> {
         let (response_tx, response_rx) = channel();
 
         trace!("Sending chunk: {:#04X}", HexSlice::new(&payload));
