d20175fa26b7b1ef48679ad768edf06e364c1e00
Richard Neumann <neumann@paulmann.de>
2026-02-23 09:28:45 +0100

    Make `Proxy` a struct.
    We only have one default implementation anyways and the inner working of `Proxy` should be kept an implementation detail.

--- src/actor/proxy.rs
+++ src/actor/proxy.rs
@@ -12,29 +12,28 @@ use crate::utils::HexSlice;
 type Response = Receiver<io::Result<()>>;
 type Error = SendError<Message>;
 
-/// Default proxy type.
-pub type DefaultProxy = Sender<Message>;
-
 /// `ASHv2` actor proxy.
-pub trait Proxy {
+#[derive(Clone, Debug)]
+pub struct Proxy {
+    inner: Sender<Message>,
+}
+
+impl Proxy {
     /// Send data to the `ASHv2` actor.
     ///
     /// # Errors
     ///
     /// Returns an [`Error`] if sending the message fails.
-    fn send(&self, payload: Payload) -> impl Future<Output = Result<Response, Error>> + Send;
-}
-
-impl Proxy for Sender<Message> {
-    async fn send(&self, payload: Payload) -> Result<Response, Error> {
+    pub async fn send(&self, payload: Payload) -> Result<Response, Error> {
         let (response_tx, response_rx) = channel();
 
         trace!("Sending chunk: {:#04X}", HexSlice::new(&payload));
-        self.send(Message::Payload {
-            payload: Box::new(payload),
-            response_tx,
-        })
-        .await?;
+        self.inner
+            .send(Message::Payload {
+                payload: Box::new(payload),
+                response_tx,
+            })
+            .await?;
 
         Ok(response_rx)
     }
