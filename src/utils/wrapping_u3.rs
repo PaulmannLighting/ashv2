c931d14b591a071cb27f081be7015f2d97a0617f
Richard Neumann <neumann@paulmann.de>
2026-05-05 13:08:21 +0200

    Update comment.

--- src/utils/wrapping_u3.rs
+++ src/utils/wrapping_u3.rs
@@ -9,7 +9,7 @@ const UNUSED_BITS: u8 = !MASK;
 
 /// A three bit number.
 // The inner `NonZero<u8>` type is used in conjunction with `#[repr(transparent)]`
-// to allow niche optimizations of this type when used in e.g. an option.
+// to allow niche optimizations of this type when used in e.g. an `Option.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[repr(transparent)]
 pub struct WrappingU3(NonZero<u8>);
